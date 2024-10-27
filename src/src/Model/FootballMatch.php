a79f06d377da558f6ce1d543a3f6bab9ab6e5179
Pavel <pavel.artjuhh@gmail.com>
2024-10-27 21:06:53 +0200

    Green: Ensure team names are valid

--- src/src/Model/FootballMatch.php
+++ src/src/Model/FootballMatch.php
@@ -3,18 +3,35 @@ declare(strict_types=1);
 
 namespace App\Model;
 
+use App\Exception\ScoreboardException;
+
 class FootballMatch
 {
 
     private int $homeScore = 0;
     private int $awayScore = 0;
 
+    private readonly string $homeTeam;
+    private readonly string $awayTeam;
+
     public function __construct(
-        private readonly string $homeTeam,
-        private readonly string $awayTeam,
+        string $homeTeam,
+        string $awayTeam,
     )
     {
+        $homeTeam = $this->sanitizeTeamName($homeTeam);
+        $awayTeam = $this->sanitizeTeamName($awayTeam);
 
+        if (empty($homeTeam)) {
+            throw new ScoreboardException('Home team name is empty');
+        }
+
+        if (empty($awayTeam)) {
+            throw new ScoreboardException('Away team name is empty');
+        }
+
+        $this->homeTeam = $homeTeam;
+        $this->awayTeam = $awayTeam;
     }
 
     public function getHomeTeam(): string
@@ -40,6 +57,11 @@ class FootballMatch
     public function getMatchStartDate(): \DateTimeImmutable
     {
 
+    }
+
+    private function sanitizeTeamName(string $teamName): string
+    {
+        return trim($teamName);
     }
 
 }
