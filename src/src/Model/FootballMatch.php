4f29e2e5e639770efbbfc978e6ed6cc90e288075
Pavel <pavel.artjuhh@gmail.com>
2024-10-28 02:00:29 +0200

    Green: Add exception for reducing score

--- src/src/Model/FootballMatch.php
+++ src/src/Model/FootballMatch.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Model;
 
 use App\Exception\ScoreboardException;
+use App\Tests\Service\ScoreboardTest;
 
 class FootballMatch
 {
@@ -63,6 +64,11 @@ class FootballMatch
     {
         if ($homeScore < 0 || $awayScore < 0) {
             throw new ScoreboardException('Team score must be 0 or greater');
+        }
+
+        /** @see ScoreboardTest::testScoreCannotBeReduced() */
+        if ($homeScore < $this->homeScore || $awayScore < $this->awayScore) {
+            throw new ScoreboardException('Score cannot be reduced');
         }
 
         $this->homeScore = $homeScore;
